03ed7c9d9e03275b1eb8d5c1bf2747052cf5eb43
Victorjoseph93 <victorjoseph001@gmail.com>
2024-07-28 00:43:33 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,70 +1,48 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
-
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * _printf - printf function
- *
- * @format: formatted string
- * Return: The total number of outputted characters
+ * _printf - Build out the printf function
+ * @format: string passed with possible format specifiers
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, j, count, find;
-	va_list list;
-	set arguments[] = {
-		{'c', print_char},
-		{'d', print_d},
-		{'i', print_d},
-		{'s', print_str},
-		{'R', print_rot13},
-		{'r', print_rev},
-	};
+	int i, blen, hlen;
+	double totalBuffer;
+	double *total;
+	va_list argp;
+	char buffer[BUFSIZE], *holder;
+	char *(*pointer_get_valid)(va_list);
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	count = 0;
-
-	for (i = 0; *(format + i); i++)
+	for (i = 0; i < BUFSIZE; i++)
 	{
-		if (*(format + i) == '%')
+		buffer[i] = 0;
+	}
+	totalBuffer = 0;
+	pointer_get_valid = NULL;
+	total = &totalBuffer;
+	va_start(argp, format);
+	for (i = blen = hlen = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
 		{
-			if (*(format + i + 1) == '\0')
-				continue;
-			find = 0;
-
-			for (j = 0; j < 6; j++)
-			{
-				if (*(format + i + 1) == arguments[j].spec)
-				{
-					count += arguments[j].print(list);
-					find = 1;
-					format++;
-					break;
-				}
-			}
-
-			if (find != 1)
-			{
-				if (*(format + i + 1) == '%')
-				{
-					count += write(1, "%", 1);
-					format++;
-				}
-				else
-					count += write(1, (format + i), 1);
-			}
-
+			pointer_get_valid = get_valid_type(format[i + 1]);
+			holder = (pointer_get_valid == NULL) ?
+				found_nothing(format[i + 1]) :
+				pointer_get_valid(argp);
+			hlen = _strlen(holder);
+			blen = alloc_buffer(holder, hlen, buffer, blen, total);
+			i++;
 		}
 		else
 		{
-			count += write(1, (format + i), 1);
+			holder = ctos(format[i]);
+			blen = alloc_buffer(holder, 1, buffer, blen, total);
 		}
 	}
-	va_end(list);
-
-	return (count);
+	va_end(argp);
+	_puts(buffer, blen);
+	return (totalBuffer + blen);
 }
